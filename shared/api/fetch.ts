9b0286cba1e9fe8ad0171060b2baaf5df78d18f8
N3side <besonovskiyyy@gmail.com>
2026-01-06 15:25:48 +0300

    app url

--- shared/api/fetch.ts
+++ shared/api/fetch.ts
@@ -22,7 +22,11 @@ export async function $fetch(
 
     const token = localStorage.getItem("token")
 
-    const url = "http://localhost:8876/api/" + route
+    const API_URL = process.env.NODE_ENV === 'production'
+        ? 'https://5fb5469c0f3e.vps.myjino.ru/api'
+        : 'http://localhost:8876/api';
+
+    const url = API_URL + route
 
     if (token) {
         headers.Authorization = "Bearer " + token
@@ -51,3 +55,5 @@ export async function $fetch(
 
     return {response,json}
 }
+
+// тут
