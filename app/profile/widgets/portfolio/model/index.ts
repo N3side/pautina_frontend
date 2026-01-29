94a8a688a9459321972bfa95416bf0a4c5a89c9d
N3side <besonovskiyyy@gmail.com>
2026-01-29 23:22:38 +0300

    feat: what did i add

--- app/profile/widgets/portfolio/model/index.ts
+++ app/profile/widgets/portfolio/model/index.ts
@@ -39,34 +39,6 @@ const cards: CardProps[] = [
         title: "ППК. Введение в алгоритмы: реализация на языке Python",
         type: "Удостоверение"
     },
-    {
-        category: "Обучение",
-        image: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/dff0/live/61c92860-24ce-11ee-941e-23d1e9ab75fa.jpg.webp",
-        date: "Ноябрь 2024",
-        title: "ППК. Введение в алгоритмы: реализация на языке Python",
-        type: "Удостоверение"
-    },
-    {
-        category: "Обучение",
-        image: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/dff0/live/61c92860-24ce-11ee-941e-23d1e9ab75fa.jpg.webp",
-        date: "Ноябрь 2024",
-        title: "ППК. Введение в алгоритмы: реализация на языке Python",
-        type: "Удостоверение"
-    },
-    {
-        category: "Обучение",
-        image: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/dff0/live/61c92860-24ce-11ee-941e-23d1e9ab75fa.jpg.webp",
-        date: "Ноябрь 2024",
-        title: "ППК. Введение в алгоритмы: реализация на языке Python",
-        type: "Удостоверение"
-    },
-    {
-        category: "Обучение",
-        image: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/dff0/live/61c92860-24ce-11ee-941e-23d1e9ab75fa.jpg.webp",
-        date: "Ноябрь 2024",
-        title: "ППК. Введение в алгоритмы: реализация на языке Python",
-        type: "Удостоверение"
-    },
 ]
 
 export {categories, cards}
