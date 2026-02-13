c6528aee84bdf55e0f495baa8f9711b7169adb6b
Neside <131708188+N3side@users.noreply.github.com>
2026-02-13 13:16:24 +0300

    Update CustomDropDown.tsx

--- src/widgets/header/ui/CustomDropDown.tsx
+++ src/widgets/header/ui/CustomDropDown.tsx
@@ -7,6 +7,9 @@ import Link from "next/link"
 import {useTheme} from "@/shared/lib/providers/ThemeProvider";
 import {IOSSwitch} from "@/shared/ui/Inputs/IOSSwitch";
 
+import {userLink} from "@/shared/lib/userLink";
+
+
 
 export default function CustomDropDown() {
 
@@ -43,7 +46,7 @@ export default function CustomDropDown() {
                                 </div>
 
                                 <div className="px-4 pb-3">
-                                    <Link href={`/profile/${user?.id}`} className="block w-full text-center py-2 rounded-lg bg-brand/10 hover:bg-brand/20 text-text-brand text-sm font-medium transition-colors">
+                                    <Link href={`$(userLink(user?.id)`} className="block w-full text-center py-2 rounded-lg bg-brand/10 hover:bg-brand/20 text-text-brand text-sm font-medium transition-colors">
                                         Перейти в профиль
                                     </Link>
                                 </div>
